24d6423f6348f8ff2d5cccb81af05f89fc084910
Omega <general.of.omega@googlemail.com>
2019-03-25 20:34:53 +0100

    "Initial commit"

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'history-statistics adapter settings': {
+        'en': 'Adapter settings for history-statistics',
+        'de': 'Adaptereinstellungen für history-statistics',
+        'ru': 'Настройки адаптера для history-statistics',
+        'pt': 'Configurações do adaptador para history-statistics',
+        'nl': 'Adapterinstellingen voor history-statistics',
+        'fr': "Paramètres d'adaptateur pour history-statistics",
+        'it': "Impostazioni dell'adattatore per history-statistics",
+        'es': 'Ajustes del adaptador para history-statistics',
+        'pl': 'Ustawienia adaptera dla history-statistics',
+        'zh-cn': 'history-statistics的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
